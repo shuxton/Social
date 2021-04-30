814b025a00aa3b3f37331e18f75fe0a319c90f85
shuxton <shubhamsnj@gmail.com>
2021-04-30 14:24:05 +0530

    Styling

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { Grid } from 'semantic-ui-react'
 import LoadingComponent from '../../../app/layout/LoadingComponents'
 import { useStore } from '../../../app/stores/store'
+import ActivityFilter from './ActivityFilter'
 
 import ActivityList from './ActivityList'
 
@@ -30,7 +31,7 @@ export default observer( function ActivityDashboard(){
             <ActivityList/>
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity filters</h2>
+                <ActivityFilter/>
             </Grid.Column>
         </Grid>
     )
