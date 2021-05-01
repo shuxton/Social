5ff3e05c20a5c1d45f2eafb0b2f6e717f0939a7e
shuxton <shubhamsnj@gmail.com>
2021-05-01 15:55:46 +0530

    Error handling

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using MediatR;
+using Application.Core;
 
 namespace API.Controllers
 {
@@ -11,5 +12,14 @@ namespace API.Controllers
         private IMediator _mediator;
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    
+        protected ActionResult HandleResult<T>(Result<T> result){
+            if(result == null)return NotFound();
+            if(result.IsSuccess && result.Value!=null)
+            return Ok(result.Value);
+            if(result.IsSuccess && result.Value==null)
+            return NotFound();
+            return BadRequest(result.Error);
+        }
     }
 }
